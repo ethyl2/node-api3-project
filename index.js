52afa2e92b00db298eb360912456aab41a47c005
Heather Nuffer <zzzuzz@gmail.com>
2020-01-04 20:28:44 -0700

    Set up express server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code away!
+const express = require('express');
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/posts', postRouter);
+server.use('/users', userRouter);
+
+const port = 9000;
+server.listen(port, () => console.log(`API running on port ${port}. Let the magic happen!`));
