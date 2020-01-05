53200ecaffe2a99144f70265dcc81ff763427227
Heather Nuffer <zzzuzz@gmail.com>
2020-01-04 21:00:43 -0700

    Added validateUserId middleware and GET routes to all users and user by id

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const server = express();
 
 server.use(express.json());
 
+/*
+logger()
+    logger logs to the console the following information about each request: request method, request url, and a timestamp
+    this middleware runs on every request made to the API
+ */
+
 function logger(req, res, next) {
     console.log(
         `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('host')}`
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userDb = require('./userDb');
 
 const router = express.Router();
 
@@ -12,10 +13,31 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  userDb.get()
+    .then(response => {
+      console.log(response);
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      //console.log(err);
+      res.status(500).json({message: 'The server was not able to get the users.', error: err});
+    });
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const id = req.body.id;
+  //const id = req.params.id;
+  userDb.getById(id)
+    .then(response => {
+      console.log(response)
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      //console.log(err);
+      res.status(500).json({message: `The server was not able to get the user of id ${id}.`, error: err});
+    });
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -32,8 +54,25 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
+/*
+validateUserId()
+
+    validateUserId validates the user id on every request that expects a user id parameter
+    if the id parameter is valid, store that user object as req.user
+    if the id parameter does not match any user id in the database, cancel the request 
+    and respond with status 400 and { message: "invalid user id" }
+*/
+
 function validateUserId(req, res, next) {
   // do your magic!
+  userDb.getById(req.body.id)
+    .then(response => {
+        req.user = response.data;
+        next();
+    })
+    .catch(err => {
+      res.status(400).json({message: "invalid user id"})
+    });
 }
 
 function validateUser(req, res, next) {
